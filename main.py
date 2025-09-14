7796236cd45498d55bf271bc3525bf2a11b418c6
Vibhum Sharma <vibhum10sharma@gmail.com>
2025-09-14 17:56:37 +0530

    "Added endpoint for series too"

--- main.py
+++ main.py
@@ -15,12 +15,34 @@ app.add_middleware(
 )
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
-TMDB_BASE_URL = "https://api.themoviedb.org/3/search/movie"
+TMDB_BASE_URL = "https://api.themoviedb.org/3"
 
+# ------------------ SEARCH ------------------
 @app.get("/search")
-def search_movies(q: str = Query(...)):
-    response = requests.get(TMDB_BASE_URL, params={
-        "api_key": TMDB_API_KEY,
-        "query": q
-    })
-    return response.json()
+def search(q: str, type: str = "movie"):
+    """
+    type = movie | tv
+    """
+    url = f"{TMDB_BASE_URL}/search/{type}"
+    params = {"api_key": TMDB_API_KEY, "query": q}
+    r = requests.get(url, params=params)
+
+    filtered = [item for item in r.get("results", []) if item.get("original_language") == "en"]
+    return {"results": filtered}
+    # return r.json()
+
+# ------------------ MOVIE DETAILS ------------------
+@app.get("/movie/{movie_id}")
+def movie_details(movie_id: int):
+    url = f"{TMDB_BASE_URL}/movie/{movie_id}"
+    params = {"api_key": TMDB_API_KEY}
+    r = requests.get(url, params=params)
+    return r.json()
+
+# ------------------ SERIES DETAILS ------------------
+@app.get("/tv/{tv_id}")
+def tv_details(tv_id: int):
+    url = f"{TMDB_BASE_URL}/tv/{tv_id}"
+    params = {"api_key": TMDB_API_KEY}
+    r = requests.get(url, params=params)
+    return r.json()
