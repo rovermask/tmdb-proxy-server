f9f26d986f82a40ce27d7c77e09d0159414c43ad
Vibhum Sharma <vibhum10sharma@gmail.com>
2025-09-14 16:36:07 +0530

    "updated"

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Query
 import requests
 import os
-from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -9,10 +8,7 @@ TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_BASE_URL = "https://api.themoviedb.org/3/search/movie"
 
 @app.get("/search")
-def search_movies(q: str = Query(..., description="Movie search query")):
-    if not q:
-        return JSONResponse({"error": "No query provided"}, status_code=400)
-
+def search_movies(q: str = Query(...)):
     response = requests.get(TMDB_BASE_URL, params={
         "api_key": TMDB_API_KEY,
         "query": q
